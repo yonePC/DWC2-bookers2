363044bf6206f972073f1172cf33fe063251d628
yonePC <ys.mailaddress0929@gmail.com>
2021-09-20 23:35:42 +0000

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correcnt_user, only: [:edit, :update]
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:info] = "successfully"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction, :profile_image_id)
+  end
+
+  def ensure_correcnt_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
