0415f90b5a13bbab36d54c0ab08c324e384e7981
yonePC <ys.mailaddress0929@gmail.com>
2021-09-20 09:39:15 +0000

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
-  resources :users, only[:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
